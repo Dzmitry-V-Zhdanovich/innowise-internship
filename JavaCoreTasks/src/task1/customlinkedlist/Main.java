c5c5356bfa1dfb251df139b6c5ea2f6ba07d818c
Dzmitry Zhdanovich <dzmitry.zhdanovich@innowise.com>
2025-10-13 15:47:08 +0300

    method addFirst implemented

---
+++ JavaCoreTasks/src/task1/customlinkedlist/Main.java
@@ -0,0 +1,7 @@
+package task1.customlinkedlist;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ JavaCoreTasks/src/task1/customlinkedlist/MyLinkedList.java
@@ -0,0 +1,33 @@
+package task1.customlinkedlist;
+
+public class MyLinkedList<T> {
+    private Node<T> head;
+    private Node<T> tail;
+    private int size;
+
+    private static class Node<T> {
+        T data;
+        Node<T> next;
+        Node<T> previous;
+
+        Node(T data) {
+            this.data = data;
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void addFirst(T item) {
+        Node<T> newNode = new Node<>(item);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            newNode.next = head;
+            head.previous = newNode;
+            head = newNode;
+        }
+        size++;
+    }
+}
