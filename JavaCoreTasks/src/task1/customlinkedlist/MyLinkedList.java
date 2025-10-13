d3b3406f2aa645f2a2b51270ddebf280d96c7929
Dzmitry Zhdanovich <dzmitry.zhdanovich@innowise.com>
2025-10-13 18:49:56 +0300

    all methods are implemented

--- JavaCoreTasks/src/task1/customlinkedlist/MyLinkedList.java
+++ JavaCoreTasks/src/task1/customlinkedlist/MyLinkedList.java
@@ -30,4 +30,121 @@ public class MyLinkedList<T> {
         }
         size++;
     }
+
+    public void addLast(T item) {
+        Node<T> newNode = new Node<>(item);
+        if (tail == null) {
+            head = tail = newNode;
+        } else {
+            newNode.previous = tail;
+            tail.next = newNode;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    public void add(int index, T item) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Invalid index " + index);
+        }
+        if (index == 0) {
+            addFirst(item);
+        } else if (index == size) {
+            addLast(item);
+        } else {
+            Node<T> current = getNode(index);
+            Node<T> newNode = new Node<>(item);
+            newNode.previous = current.previous;
+            newNode.next = current;
+            current.previous.next = newNode;
+            current.previous = newNode;
+            size++;
+        }
+    }
+
+    public T getFirst() {
+        if (head == null) {
+            throw new IllegalStateException("List is empty");
+        } else {
+            return head.data;
+        }
+    }
+
+    public T getLast() {
+        if (tail == null) {
+            throw new IllegalStateException("List is empty");
+        } else {
+            return tail.data;
+        }
+    }
+
+    public T get(int index) {
+        return getNode(index).data;
+    }
+
+    public T removeFirst() {
+        if (head == null) {
+            throw new IllegalStateException("List is empty");
+        }
+        T data = head.data;
+        head = head.next;
+        if (head != null) {
+            head.previous = null;
+        } else {
+            tail = null;
+        }
+        size--;
+        return data;
+    }
+
+    public T removeLast() {
+        if (tail == null) {
+            throw new IllegalStateException("List is empty");
+        }
+        T data = tail.data;;
+        tail = tail.previous;
+        if (tail != null) {
+            tail.next = null;
+        } else {
+            head = null;
+        }
+        size--;
+        return data;
+    }
+
+    public T remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Invalid index " + index);
+        }
+        if (index == 0) {
+            return removeFirst();
+        } else if (index == size - 1) {
+            return removeLast();
+        } else {
+            Node<T> current = getNode(index);
+            current.previous.next = current.next;
+            current.next.previous = current.previous;
+            size--;
+            return current.data;
+        }
+    }
+
+    private Node<T> getNode(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Invalid index " + index);
+        }
+        Node<T> current;
+        if (index < size / 2) {
+            current = head;
+            for (int i = 0; i < index; i++) {
+                current = current.next;
+            }
+        } else {
+            current = tail;
+            for (int i = size - 1; i > index; i--) {
+                current = current.previous;
+            }
+        }
+        return current;
+    }
 }
